db8416ad7f6314bc9e75e06ee060110f646bdad1
Pearce Haviland <pearce.haviland@protonmail.com>
2021-05-14 00:08:30 -0400

    Add project files.

---
+++ GameOfWar/Deck.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameOfWar
+{
+    class Deck
+    {
+        // Values associated with each card, from lowest to highest
+        //
+        // By using an enum, this allows face cards to be compared with numerical values
+        // in a simple and readable way.
+        public enum CardValue
+        {
+            TWO,
+            THREE,
+            FOUR,
+            FIVE,
+            SIX,
+            SEVEN,
+            EIGHT,
+            NINE,
+            TEN,
+            JACK,
+            QUEEN,
+            KING,
+            ACE
+        }
+        
+        // Suits for each card
+        //
+        // Used for assigning names to cards on initialization
+        public enum CardSuit
+        {
+            CLUBS,
+            SPADES,
+            DIAMONDS,
+            HEARTS
+        }
+
+        struct Card
+        {
+            CardValue val;  // Value or "rank" of card, to be compared against other cards
+            string name;    // Display name for the card
+        }
+        
+        // Constructor
+        public Deck()
+        {
+            /*
+             * Initialize each card
+             * Add each card to deck
+             */
+        }
+
+        // Shuffle
+        // Takes the current deck and randomizes the location of each card
+        public void Shuffle()
+        {
+            int cardCount = m_cards.Count;
+
+            // Initialize random number generator
+            Random randGenerator = new Random();
+            
+            // Randomized position for shuffling
+            int randPos;
+
+            for (int i = 0; i < cardCount; i++)
+            {
+                // Generate random position
+                randPos = randGenerator.Next(cardCount);
+
+                swapCardsAt(i, randPos);
+            }
+        }
+
+        // swapCardsAt
+        // Given two card locations, switches their location in the deck
+        // 
+        // param first - the position of the first card to be switched
+        // param second - the position of the second card to be switched
+        private void swapCardsAt(int first, int second)
+        {
+            int cardCount = m_cards.Count;
+
+            // Only perform the operation if given valid positions, and the positions
+            // are NOT the same
+            if (first < cardCount && second < cardCount && first != second)
+            {
+                // Placeholder for first card when swapping
+                Card temp;
+                temp = m_cards[first];
+
+                // Swap cards to each other's positions
+                m_cards[first] = m_cards[second];
+                m_cards[second] = temp;
+            }
+        }
+
+        private List<Card> m_cards;  // Deck of cards for shuffling, dealing, etc.
+    }
+}
---
+++ GameOfWar/GameOfWar.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+// Each player gets dealt half the deck, 26 cards, and the cards are put face down
+// in a stack in front of the players.
+//
+// Both players turn their top card face up at the same time. The person with the
+// higher card wins that draw, and takes both the cards. They are put to the side
+// to form a new stack, which the player can use when he finishes his current stack.
+//
+// If both players draw a card of the same rank, e.g. they both draw 8s, then there's
+// a war. The face up cards are left on the table and each player puts three cards face
+// down on the table, and then puts one card face up. The face up card determines who
+// wins the war and gets all 10 cards that are on the table at this point. If the face
+// up card is again the same rank, then the war goes on, three more face down, one face up etc.
+//
+// First player to finish all their cards loses the game.
+//
+// If a player finishes their cards during a war without having enough cards to finish the
+// war then they lose immediately.
+
+namespace GameOfWar
+{
+    class GameOfWar
+    {
+    }
+}
