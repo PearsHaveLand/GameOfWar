773e50ebaa06d8f0a5753465bfcc757cf6ffb780
Pearce Haviland <pearce.haviland@protonmail.com>
2021-05-19 10:32:48 -0400

    Removed unnecessary testing functions

--- GameOfWar/Program.cs
+++ GameOfWar/Program.cs
@@ -4,44 +4,6 @@ namespace GameOfWar
 {
     class Program
     {
-        static void Split()
-        {
-            Deck deck1 = new Deck();
-            Deck deck2 = deck1.Split();
-
-            Console.WriteLine("Deck 1:");
-            deck1.Shuffle();
-            Console.WriteLine(deck1.ToString());
-
-            Console.WriteLine("Deck 2:");
-            Console.WriteLine(deck2.ToString());
-        }
-
-        static void PrintShuffle()
-        {
-            Deck mydeck = new Deck();
-            mydeck.ToString();
-            Console.WriteLine("-------------------");
-            mydeck.Shuffle();
-            mydeck.ToString();
-        }
-
-        static void Draw()
-        {
-            Deck mydeck = new Deck();
-            Card? card = mydeck.DrawCard();
-            while (card is Card valueOfCard)
-            {
-                Console.WriteLine(valueOfCard.ToString());
-                card = mydeck.DrawCard();
-            }
-
-            if (!(card is Card cardVal))
-            {
-                Console.WriteLine("End of the deck");
-            }
-        }
-
         static void Main(string[] args)
         {
             GameOfWar game = new GameOfWar();
